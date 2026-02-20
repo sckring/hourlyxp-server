7cb91ad824f70814bdf7f9eaef0290f1c6170757
sckring <44453476+sckring@users.noreply.github.com>
2026-02-20 09:01:21 -0500

    fix port and start script

--- server.js
+++ server.js
@@ -53,6 +53,8 @@ app.post("/send", async (req, res) => {
 
 /* ============================= */
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
